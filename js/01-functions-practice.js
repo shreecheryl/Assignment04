e1b22f8e70e2f05000cc749e1d6cc910cb346a3d
shreecheryl <shreecheryl@att.net>
2023-03-03 13:40:59 -0800

    initial commit

---
+++ js/01-functions-practice.js
@@ -0,0 +1,21 @@
+//STEP 1
+
+function halfNumber(num) {
+    console.log(`Half of ${num} is ${(num / 2).toFixed(2)}.`);
+}
+halfNumber(parseFloat(prompt("Enter a number")));
+
+//STEP 2
+
+function squareNumber(num) {
+    console.log(`The result of squaring the number ${num} is ${num * num}.`);
+}
+squareNumber(parseFloat(prompt("Enter a number")));
+
+//STEP 3
+
+
+
+//STEP 4
+
+//STEP 5
