5625a0e8e78ba97e8d9dd17bc8e8d317b887dc05
shreecheryl <shreecheryl@att.net>
2023-03-03 15:33:44 -0800

    Functions Practice completed

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -10,12 +10,29 @@ halfNumber(parseFloat(prompt("Enter a number")));
 function squareNumber(num) {
     console.log(`The result of squaring the number ${num} is ${num * num}.`);
 }
-squareNumber(parseFloat(prompt("Enter a number")));
+squareNumber(parseInt(prompt("Enter a number")));
 
 //STEP 3
 
-
+function percentOf() {
+    let x = parseInt(prompt("Enter a number"));
+    let y = parseInt(prompt("Enter a second number"));
+    let percentage = ((x / y) * 100).toFixed(2);
+    console.log(`${x} is ${percentage}% of ${y}.`);
+}
+percentOf();
 
 //STEP 4
+
+function findModulus() {
+    let x = parseInt(prompt("Enter a number"));
+    let y;
+    do {
+        y = parseInt(prompt("Enter a second number smaller than the first"));
+    } while (y > x);
+    let modulus = x % y;
+    console.log(`${modulus} is the modulus of ${x} and ${y}.`);
+}
+findModulus();
 
 //STEP 5
