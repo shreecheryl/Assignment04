e7b8831f7aecf358a96e400b9b3efe53f1faa576
shreecheryl <shreecheryl@att.net>
2023-03-05 18:26:39 -0800

    Basic Calculator working

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,8 +1,32 @@
 // IMPORT THE MODULE
+import {add, subtract, multiply, divide} from './modules/calculator.js'
 
+let x, y, operation;
+
+do {
 // COLLECT FIRST NUMBER FROM USER
+    x = parseFloat(prompt("Enter a number"));
+
 // COLLECT SECOND NUMBER FROM USER
+    y = parseFloat(prompt("Enter a second number"));
+
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+    operation = prompt("What operation do you want to perform? (+, -, * or /)");
+
+} while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+switch (operation) {
+    case "+":
+        add(x,y);
+        break;
+    case "-":
+        subtract(x,y);
+        break;
+    case "*":
+        multiply(x,y);
+        break;
+    default:
+        divide(x,y);
+}
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,31 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+function calculate(x, y, z, calc) {
+    alert(`${x} ${calc} ${y} = ${z}`)
+}
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
-
+function add(x,y) {
+    let z = x + y;
+    calculate(x,y,z,"+");
+}
 // SUBTRACT FUNCTION
-
+function subtract(x,y) {
+    let z = x - y;
+    calculate(x,y,z,"-");
+}
 // MULTIPLY FUNCTION
+function multiply(x,y) {
+    let z = x * y;
+    calculate(x,y,z,"*");
+}
 
 // DIVIDE FUNCTION
+function divide(x,y) {
+    let z = x / y;
+    calculate(x,y,z,"/");
+}
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add, subtract, multiply, divide}
